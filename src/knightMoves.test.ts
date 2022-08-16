09ec839c884d8f6ab416b68b3fa41bfc29ed5763
nickslick03 <nicholasepps03@gmail.com>
2022-08-16 10:07:49 -0400

    created knights trevails

--- src/knightMoves.test.ts
+++ src/knightMoves.test.ts
@@ -1,9 +1,17 @@
-import { knightMoves } from "./knightMoves"
+import {  pointOutOfBounds, pointsOverlap, getPoints, knightMoves } from "./knightMoves";
 
+test('knightMoves helper Functions', () => {
+    
+    expect(pointsOverlap([1, 1], [1, 1])).toBe(true);
+    expect(pointsOverlap([1, 1], [1, 2])).toBe(false);
 
+    expect(pointOutOfBounds([0, 0])).toBe(false);
+    expect(pointOutOfBounds([8, 0])).toBe(true);
+
+    console.log(getPoints([[0, 0], [1, 2]]));
+});
 
 test('knightMoves', () => {
-    expect(knightMoves([0, 0], [0, 0])).toEqual([[0, 0], [0, 0]]);
-
-    expect(knightMoves([3,3],[0,0])).toEqual([[3,3],[1,2],[0,0]]);
-})
+    expect(knightMoves([0, 0], [1, 2])).toEqual([[0, 0], [1, 2]]);
+    console.log(knightMoves([0, 0], [1, 3]));
+});
--- src/knightMoves.ts
+++ src/knightMoves.ts
@@ -1,19 +1,41 @@
 type chessPoint = [number, number];
 
-export const knightMoves = (startPoint: chessPoint, endPoint: chessPoint): chessPoint[] => {
-    const path = findPath([startPoint], endPoint);
 
+const knightMoves = (startPoint: chessPoint, endPoint: chessPoint): chessPoint[] => {
+    let path: chessPoint[] = [];
+    const findPath = (currentPath: chessPoint[]) => {
+        if(path.length > 0) return;
+        if(pointsOverlap(endPoint, currentPath[currentPath.length - 1])) {
+            path = currentPath;
+            return;
+        }
+        const nextPoints = getPoints(currentPath);
+        const nextPaths = nextPoints.map(point => currentPath.concat([point]));
+        nextPaths.forEach(path => findPath(path));
+    };
+    findPath([startPoint]);
     return path;
-}
-
-const findPath = (currentPath: chessPoint[], endPoint: chessPoint): chessPoint[] => {
-    if(pointsOverlap(currentPath[currentPath.length - 1], endPoint)) return currentPath;
-    
-}
+};
 
 const pointsOverlap = (point1: chessPoint, point2: chessPoint) => 
     point1[0] === point2[0] && point1[1] === point2[1];
 
 const pointOutOfBounds = (point: chessPoint) =>
-    point.every(coord => coord >= 0 && coord <= 7);
+    point.some(coord => coord < 0 || coord > 7);
 
+const getPoints = (path: chessPoint[]) => {
+    const currentPoint = path[path.length - 1];
+    const nextPoints: chessPoint[] = [];
+    for(let x = -2; x <= 2; x ++) {
+        for(let y = -2; y <= 2; y ++) {
+            if(x === 0 || y === 0 || Math.abs(x) === Math.abs(y)) continue;
+            const newPoint: chessPoint = [currentPoint[0] + x, currentPoint[1] + y];
+            if(pointOutOfBounds(newPoint) || path.some(point => pointsOverlap(point, newPoint))) 
+                continue;
+            nextPoints.push(newPoint);
+        }
+    }
+    return nextPoints;
+};
+
+export { knightMoves, pointOutOfBounds, pointsOverlap, getPoints };
